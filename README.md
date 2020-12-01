f19d8e270a0537417165f9cb3123044a94d36478
Luke Ledet <luke@revelry.co>
2020-12-01 17:49:08 -0600

    Travis setup and quick readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Tunnel
+
+This is an app that runs in docker to easily provide a reverse proxy similar to ngrok using your own domain and authentication using public keys associated with users of a Github organization.
---
+++ bin/docker_push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+HEAD_SHA=$(git rev-parse --short HEAD)
+
+IMAGE_TAG=$DOCKER_HOST/$DOCKER_REPO
+LATEST_TAG=$IMAGE_TAG:$TRAVIS_BRANCH-latest
+VERSIONED_TAG=$IMAGE_TAG:$TRAVIS_BRANCH-$HEAD_SHA
+
+docker pull $LATEST_TAG || true
+docker build --cache-from $LATEST_TAG -t $VERSIONED_TAG . || exit 1
+
+docker build --cache-from $LATEST_TAG \
+  --tag $VERSIONED_TAG \
+  --build-arg github_token="$GITHUB_TOKEN" \
+  --build-arg github_org="$GITHUB_ORG" \
+  --build-arg host_keys="$HOST_KEYS" . || exit 1
+
+docker push $VERSIONED_TAG || exit 1
+docker tag $VERSIONED_TAG $LATEST_TAG
+docker push $LATEST_TAG
